e8f7fbbf0dc15f1068252f8de602b25f7c754313
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-15 05:32:21 +0000

    ginkgo: NAD-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/qassa_ginkgo.mk
+    $(LOCAL_DIR)/nad_ginkgo.mk
--- nad_ginkgo.mk
+++ nad_ginkgo.mk
@@ -25,11 +25,10 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 # Inherit some common ArrowOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_PIXEL_CHARGING := true
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/qassa/config/common_full_phone.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := qassa_ginkgo
+PRODUCT_NAME := nad_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
